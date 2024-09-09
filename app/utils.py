57f3ae187b559718f8f1d9a71fb2b14b2606c447
李庭均 <san222631@gmail.com>
2024-09-09 05:04:08 +0200

    test

--- app/utils.py
+++ app/utils.py
@@ -105,5 +105,5 @@ def fetch_CAT_data(page: int, keyword: str):
 #推薦商品
 def get_redis_client():
     # Initialize Redis client
-    redis_client = redis.Redis(host='localhost', port=6379, db=0)
+    redis_client = redis.Redis(host='172.31.47.196', port=6379, db=0)
     return redis_client
